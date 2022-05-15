e16b53975d8df29778dc2e668353bcef0c32c775
Geda-999 <1322001816@qq.com>
2022-05-15 20:35:54 +0800

    初始化tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 导入tailwind.css库
+import "./tailwind.css";
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="flex items-center justify-center">
+        <img class="w-40 h-40" alt="Vue logo" src="../assets/logo.png" />
+    </div>
+    
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+
+    <div class="text-komabig">小马大字体</div>
+    <div class="bg-bootstrap_primary text-white">像我么</div>
+    <div class="bg-bootstrap_success text-white">像我么</div>
+    <div class="bg-bootstrap_info text-white">像我么</div>
+    <div class="bg-red-600 h-screen"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '../components/HelloWorld.vue'
+import HelloWorld from "../components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
