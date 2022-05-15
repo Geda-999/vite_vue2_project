499b99c169de35390883570f9730a61d29e8b1fa
Geda-999 <1322001816@qq.com>
2022-05-15 19:23:22 +0800

    初始化element-ui

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
+
+// 导入引入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 这是按需导入了
+// 按需就要安装插件先 yarn add babel-plugin-component -D
+// import './plugins/element.js'
+
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+
+// 全局引入element-ui
+Vue.use(ElementUI);
 
 new Vue({
   router,
