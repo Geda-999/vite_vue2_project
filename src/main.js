5bc3e0fc4d60ccffd5e7edb590e73ebfe66d85e5
Geda-999 <1322001816@qq.com>
2022-05-15 19:30:41 +0800

    初始化element-ui

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
 // 这是按需导入了
+// 链接 https://element.eleme.cn/#:~:text=%C2%B6-,%E6%8C%89%E9%9C%80%E5%BC%95%E5%85%A5,-%E5%80%9F%E5%8A%A9%20babel%2Dplugin
+// 在根目录上 创建 babel.config.js
 // 按需就要安装插件先 yarn add babel-plugin-component -D
 // import './plugins/element.js'
 
