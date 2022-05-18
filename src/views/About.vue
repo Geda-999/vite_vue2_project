fb5c9dba0679c75d01a615ca9b62c19d6ce441c7
Geda-999 <1322001816@qq.com>
2022-05-18 23:38:04 +0800

    初始化

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,26 @@
 		<h1>This is an about page</h1>
 		<p>Hello 这里是初始化 <b> vite + vue2 </b>啦！！！</p>
 		<div><i-mdi-account-reactivate style="font-size: 2em; color: red" /></div>
+		<div class="a"></div>
 	</div>
 </template>
+<style lang="scss">
+.a {
+	width: 100px;
+	height: 100px;
+	display: flex;
+	margin: 0 auto;
+	background-color: #f0f;
+
+	//&是父选择器标识符
+	&:hover {
+		background: red;
+	}
+
+	//属性嵌套
+	border: 1px solid #000 {
+		left: 5px solid #000;
+		right: 10px solid #000;
+	}
+}
+</style>
