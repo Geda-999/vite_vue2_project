2e9b825d5a9b90c058dc559e821b00ccbfab8503
Geda-999 <1322001816@qq.com>
2022-05-18 22:44:54 +0800

    初始化

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,5 +3,20 @@
 		<h1>This is an about page</h1>
 		<p>Hello 这里是初始化 <b> vite + vue2 </b>啦！！！</p>
 		<div><i-mdi-account-reactivate style="font-size: 2em; color: red" /></div>
+		<div
+			class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4"
+		>
+			<div class="shrink-0">
+				<img
+					class="h-20 w-20 block mx-auto rounded-full sm:mx-0 sm:shrink-0"
+					src="../assets/logo.png"
+					alt="ChitChat Logo"
+				/>
+			</div>
+			<div>
+				<div class="text-xl font-medium text-black">ChitChat</div>
+				<p class="text-slate-500">You have a new message!</p>
+			</div>
+		</div>
 	</div>
 </template>
